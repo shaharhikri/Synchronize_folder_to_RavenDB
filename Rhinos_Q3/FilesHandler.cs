b8684109860ed2a8a582c3a6dfa3e34b3c03a4da
Shahar <shaharhikri@gmail.com>
2021-12-07 14:52:22 +0200

    Arguments are passed to the main by the command line

--- Rhinos_Q3/FilesHandler.cs
+++ Rhinos_Q3/FilesHandler.cs
@@ -1,143 +1,151 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-public class FilesHandler
+namespace Rhinos_Q3
 {
-    private string _files_folder_path;
-    private string _last_run_date_path;
-    private string _last_run_ids_path;
-    private static readonly string FILE_TYPE = "json";
-
-    private List<string> _modified_files_names;
-    private List<string> _deleted_ids;
-
-    private List<string> _file_ids;
-
-    public FilesHandler(string files_folder_path, string last_run_date_path, string last_run_ids_path)
+    public class FilesHandler
     {
-        if (files_folder_path == null || last_run_date_path == null || last_run_ids_path == null)
+        private string _files_folder_path; 
+        private string _last_run_date_path; //Cache file for last execution date.
+        private string _last_run_ids_path;  //Cache file for last run files ids.
+        private static readonly string FILE_TYPE = "json";
+
+        private List<string> _modified_files_names; //names of jsons(files) that were modified AFTER last run.
+        private List<string> _deleted_ids;          //id's of jsons(files) that were deleted AFTER last run.
+
+        private List<string> _file_ids;
+
+        public FilesHandler(string files_folder_path, string last_run_date_path, string last_run_ids_path)
         {
-            throw new ArgumentNullException();
+            if (files_folder_path == null || last_run_date_path == null || last_run_ids_path == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this._files_folder_path = files_folder_path;
+            this._last_run_date_path = last_run_date_path;
+            this._last_run_ids_path = last_run_ids_path;
+
+            Init();
+
+            //Print _deleted_ids and _modified_files_names after init
+            /*
+            Console.WriteLine("deleted ids:");
+            foreach (string id in _deleted_ids)
+            {
+                Console.WriteLine(id);
+            }
+
+            Console.WriteLine("modified files:");
+            foreach (string m in _modified_files_names)
+            {
+                Console.WriteLine(m);
+            }
+            */
         }
 
-        this._files_folder_path = files_folder_path;
-        this._last_run_date_path = last_run_date_path;
-        this._last_run_ids_path = last_run_ids_path;
-
-        Init();
-
-        Console.WriteLine("deleted ids:");
-        foreach(string id in _deleted_ids)
+        public List<string> GetModifiedFilesNames()
         {
-            Console.WriteLine(id);
+            return _modified_files_names;
         }
 
-        Console.WriteLine("modified files:");
-        foreach (string m in _modified_files_names)
+        public List<string> GetDeletedIDs()
         {
-            Console.WriteLine(m);
-        }
-    }
-
-    public List<string> GetModifiedFilesNames()
-    {
-        return _modified_files_names;
-    }
-
-    public List<string> GetDeletedIDs()
-    {
-        return _deleted_ids;
-    }
-
-    private void Init()
-    {
-        List<string> file_names;
-        List<string> last_file_ids;
-
-        try
-        {
-            file_names = Directory.GetFiles(_files_folder_path, "*." + FILE_TYPE).ToList();
-        }
-        catch (System.IO.FileNotFoundException)
-        {
-            file_names = new List<string>();
+            return _deleted_ids;
         }
 
-        try
+        private void Init()
         {
-            last_file_ids = File.ReadLines(_last_run_ids_path).ToList();
-        }
-        catch (System.IO.FileNotFoundException)
-        {
-            last_file_ids = new List<string>();
-        }
-
-        List<string> file_names_clone = new List<string>(file_names);
-        _file_ids = new List<string>();
-        foreach (string path in file_names_clone)
-        {
-            string json = File.ReadAllText(path, System.Text.Encoding.UTF8);
+            List<string> file_names;
+            List<string> last_file_ids;
 
             try
             {
-                JObject jo = JObject.Parse(json); // JsonReaderException
-                JToken id_token = jo["Id"];
-                if (id_token != null)
+                file_names = Directory.GetFiles(_files_folder_path, "*." + FILE_TYPE).ToList();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                file_names = new List<string>();
+            }
+
+            try
+            {
+                last_file_ids = File.ReadLines(_last_run_ids_path).ToList();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                last_file_ids = new List<string>();
+            }
+
+            List<string> file_names_clone = new List<string>(file_names);
+            _file_ids = new List<string>();
+            foreach (string path in file_names_clone)
+            {
+                string json = File.ReadAllText(path, System.Text.Encoding.UTF8);
+
+                try
                 {
-                    string id = jo["Id"].ToString();
-                    _file_ids.Add(id);
+                    JObject jo = JObject.Parse(json); // JsonReaderException
+                    JToken id_token = jo["Id"];
+                    if (id_token != null)
+                    {
+                        string id = jo["Id"].ToString();
+                        _file_ids.Add(id);
+                    }
+                    else
+                    {
+                        file_names.Remove(path);
+                    }
+
                 }
-                else
+                catch (JsonReaderException)
                 {
-                    file_names.Remove(path);
+                    continue;
                 }
-
             }
-            catch (JsonReaderException)
+
+
+            //Init _deleted_ids
+            _deleted_ids = new List<string>();
+            foreach (string id in last_file_ids)
             {
-                continue;
+                if (!_file_ids.Contains(id))
+                {
+                    _deleted_ids.Add(id);
+                }
             }
-        }
-        
 
-        //Init _deleted_ids
-        _deleted_ids = new List<string>();
-        foreach (string id in last_file_ids)
-        {
-            if (!_file_ids.Contains(id))
+            //Init _modified_files_names
+            DateTime last_run_date; //if last_time_date.txt is exist and include Datetime - last_run_date equals this Datetime, else last_run_date equals the oldest Datetime
+            try
             {
-                _deleted_ids.Add(id);
+                last_run_date = DateTime.Parse(File.ReadAllText(_last_run_date_path));
             }
-        }
-
-        //Init _modified_files_names
-        DateTime last_run_date; //if last_time_date.txt is exist and include Datetime - last_run_date equals this Datetime, else last_run_date equals the oldest Datetime
-        try
-        {
-            last_run_date = DateTime.Parse(File.ReadAllText(_last_run_date_path));
-        }
-        catch (System.IO.FileNotFoundException)
-        {
-            last_run_date = DateTime.MinValue; //oldest date
-        }
-        catch (FormatException)
-        {
-            last_run_date = DateTime.MinValue; //oldest date
-        }
-        _modified_files_names = new List<string>();
-        foreach (var fn in file_names)
-        {
-            DateTime f_modied_date = System.IO.File.GetLastWriteTime(fn);
-            if (last_run_date < f_modied_date)
+            catch (System.IO.FileNotFoundException)
             {
-                _modified_files_names.Add(fn);
+                last_run_date = DateTime.MinValue; //oldest date
+            }
+            catch (FormatException)
+            {
+                last_run_date = DateTime.MinValue; //oldest date
+            }
+            _modified_files_names = new List<string>();
+            foreach (var fn in file_names)
+            {
+                DateTime f_modied_date = System.IO.File.GetLastWriteTime(fn);
+                if (last_run_date < f_modied_date)
+                {
+                    _modified_files_names.Add(fn);
+                }
             }
         }
-    }
 
-    public void UpdateCacheFiles()
-    {
-        File.WriteAllText(_last_run_date_path, DateTime.Now.ToString()); //update last_run_date.txt file
-        File.WriteAllLines(_last_run_ids_path, _file_ids);
+        /*Updates Cache files ("last_run_date.txt", "last_run_ids.txt") for the next run after sync folder.*/
+        public void UpdateCacheFiles()
+        {
+            File.WriteAllText(_last_run_date_path, DateTime.Now.ToString()); //update last_run_date.txt file
+            File.WriteAllLines(_last_run_ids_path, _file_ids);
+        }
     }
 }
+
--- Rhinos_Q3/Program.cs
+++ Rhinos_Q3/Program.cs
@@ -9,129 +9,162 @@ using Raven.Client.Exceptions.Documents;
 using Raven.Client.Http;
 using Sparrow.Json;
 
-public class Program
+namespace Rhinos_Q3
 {
-    public static readonly string LAST_RUN_DATE_PATH = "../../../last_run_date.txt";
-    public static readonly string LAST_RUN_IDS_PATH = "../../../last_run_ids.txt";
-    public enum CommandType { PUT, DELETE };
-
-    public static void Main(string[] args)
+     public class Program
     {
-        //Main args
-        string serverURL = "http://live-test.ravendb.net";
-        string databaseName = "ShaharHikriDB";
-        string folder_path = "C:/Users/Shahar/Desktop/";
+        //Program cache data files paths.
+        //If you change sync folder dir to a new one after first use, You should delete those files before next run. 
+        public static readonly string LAST_RUN_DATE_PATH = "../../../last_run_date.txt";
+        public static readonly string LAST_RUN_IDS_PATH = "../../../last_run_ids.txt";
 
-        FilesHandler f_handler = new FilesHandler(folder_path, LAST_RUN_DATE_PATH, LAST_RUN_IDS_PATH);
-
-        Console.WriteLine("");
-        if (updatesFilesInDB(serverURL, databaseName, f_handler.GetModifiedFilesNames(), f_handler.GetDeletedIDs()))
+        public static void Main(string[] args)
         {
-            f_handler.UpdateCacheFiles();
-            Console.WriteLine("\'" + databaseName + "\' DB Update succeded , OR There's no update to do in DB...");
-        }
-        else
-        {
-            Console.WriteLine("Cannot connect to DB \'" + databaseName + "\'...");
-        }
-    }
-
-    public static IDocumentStore createDocumentStore(string serverURL, string databaseName)
-    {
-        IDocumentStore documentStore = new DocumentStore
-        {
-            Urls = new[] { serverURL },
-            Database = databaseName
-        };
-
-        documentStore.Initialize();
-        return documentStore;
-    }
-
-    public static bool updatesFilesInDB(string serverURL, string databaseName, List<string> modified_files_names, List<string> deleted_ids)
-    {
-        try
-        {
-            using (IDocumentStore documentStore = createDocumentStore(serverURL, databaseName))
+            if(args.Length < 3)
             {
-                using (IDocumentSession session = documentStore.OpenSession(databaseName))
-                {
-                    updateDocsInDB(session, modified_files_names);
-                    deleteDocsFromDB(session, deleted_ids);
-                    session.SaveChanges();
-                }
+                Console.WriteLine("Not enough arguments for the program.");
+                return;
             }
-            return true;
-        }
-        catch (AggregateException e)
-        {
-            return false;
-        }
-        catch (DatabaseDoesNotExistException e)
-        {
-            return false;
-        }
-    }
 
-    public static void deleteDocsFromDB(IDocumentSession session, List<string> deleted_ids)
-    {
-        foreach (string id in deleted_ids)
-        {
-            var command = new DeleteDocumentCommand(id, null);
-            session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
-            Console.WriteLine("Document - id:\'" + id + "\' deleted.\n");
-        }
-    }
+            //Main args
+            string serverURL = args[1];
+            string databaseName = args[2];
+            string folder_path = args[0];
+            /*
+            Passed with VS Community args to main as "C:/Users/Shahar/Desktop/ http://live-test.ravendb.net ShaharHikriDB"
+            Right click on empty space of "Solution explorer" -> "Debug" -> "General" -> "Open debug launch prifiles UI" -> "Command Line arguments" 
+            Insted of those hard coded parameters:
+            serverURL = "http://live-test.ravendb.net";
+            databaseName = "ShaharHikriDB";
+            folder_path = "C:/Users/Shahar/Desktop/";
+            */
 
-    public static void updateDocsInDB(IDocumentSession session, List<string> jsons_files_names)
-    {
-        string json;
-        JObject jo;
-        JToken id_token;
-        string id;
-        foreach (string modified_file_path in jsons_files_names)
-        {
-            json = File.ReadAllText(modified_file_path, System.Text.Encoding.UTF8);
+            FilesHandler f_handler;// = new FilesHandler(folder_path, LAST_RUN_DATE_PATH, LAST_RUN_IDS_PATH);
 
             try
             {
-                jo = JObject.Parse(json); // JsonReaderException
-                id_token = jo["Id"];
-                if (id_token != null)
-                {
-                    id = jo["Id"].ToString();
-                    //Console.WriteLine(id);
-                    var blittableJson = ParseJson(session.Advanced.Context, json);
-                    var command = new PutDocumentCommand(id, null, blittableJson);
-                    session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context); //DocumentCollectionMismatchException
-                    Console.WriteLine("Json \'" + modified_file_path + "\' created/updated.\n");
-                }
-                else
-                {
-                    Console.WriteLine("Json \'" + modified_file_path + "\' has no \'Id\',\n");
-                }
+                f_handler = new FilesHandler(folder_path, LAST_RUN_DATE_PATH, LAST_RUN_IDS_PATH);
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                Console.WriteLine("Folder To Sync didn't found.");
+                return;
+            }
 
-            }
-            catch (JsonReaderException)
+            Console.WriteLine("");
+            if (syncFilesOnDB(serverURL, databaseName, f_handler.GetModifiedFilesNames(), f_handler.GetDeletedIDs()))
             {
-                Console.WriteLine("Json " + modified_file_path+" format is corrupted ,and cannot sync (UPDATE/CREATE) with DB.");
+                f_handler.UpdateCacheFiles();
+                Console.WriteLine("\'" + databaseName + "\' DB Update succeded , OR There's no update to do in DB...");
             }
-            catch (DocumentCollectionMismatchException)
+            else
             {
-                Console.WriteLine("Json has no \'@metadata\' attribute  ,and cannot sync (UPDATE/CREATE) with DB.");
+                Console.WriteLine("Cannot connect to DB \'" + databaseName + "\'...");
             }
         }
-    }
 
-    public static BlittableJsonReaderObject ParseJson(JsonOperationContext context, string json)
-    {
-        using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+        public static IDocumentStore createDocumentStore(string serverURL, string databaseName)
         {
-            ValueTask<BlittableJsonReaderObject> b = context.ReadForMemoryAsync(stream, "json");
-            return b.Result;
+            IDocumentStore documentStore = new DocumentStore
+            {
+                Urls = new[] { serverURL },
+                Database = databaseName
+            };
+
+            documentStore.Initialize();
+            return documentStore;
+        }
+
+        //Syncing the folder on DB
+        public static bool syncFilesOnDB(string serverURL, string databaseName, List<string> modified_files_names, List<string> deleted_ids)
+        {
+            try
+            {
+                using (IDocumentStore documentStore = createDocumentStore(serverURL, databaseName))
+                {
+                    using (IDocumentSession session = documentStore.OpenSession(databaseName))
+                    {
+                        updateDocsInDB(session, modified_files_names);
+                        deleteDocsFromDB(session, deleted_ids);
+                        session.SaveChanges();
+                    }
+                }
+                return true;
+            }
+            catch (AggregateException e)
+            {
+                return false;
+            }
+            catch (DatabaseDoesNotExistException e)
+            {
+                return false;
+            }
+        }
+
+        //Delete json's fitting docs in DB.
+        public static void deleteDocsFromDB(IDocumentSession session, List<string> deleted_ids)
+        {
+            foreach (string id in deleted_ids)
+            {
+                var command = new DeleteDocumentCommand(id, null);
+                session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
+                Console.WriteLine("Document - id:\'" + id + "\' deleted.\n");
+            }
+        }
+
+        //Create/Update json's fitting docs (with the same id) in DB.
+        public static void updateDocsInDB(IDocumentSession session, List<string> jsons_files_names)
+        {
+            string json;
+            JObject jo;
+            JToken id_token;
+            string id;
+            foreach (string modified_file_path in jsons_files_names)
+            {
+                json = File.ReadAllText(modified_file_path, System.Text.Encoding.UTF8);
+
+                try
+                {
+                    jo = JObject.Parse(json); // JsonReaderException
+                    id_token = jo["Id"];
+                    if (id_token != null)
+                    {
+                        id = jo["Id"].ToString();
+                        //Console.WriteLine(id);
+                        var blittableJson = ParseJson(session.Advanced.Context, json);
+                        var command = new PutDocumentCommand(id, null, blittableJson);
+                        session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context); //DocumentCollectionMismatchException
+                        Console.WriteLine("Json \'" + modified_file_path + "\' created/updated.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Json \'" + modified_file_path + "\' has no \'Id\',\n");
+                    }
+
+                }
+                catch (JsonReaderException)
+                {
+                    Console.WriteLine("Json " + modified_file_path+" format is corrupted ,and cannot sync (UPDATE/CREATE) with DB.");
+                }
+                catch (DocumentCollectionMismatchException)
+                {
+                    Console.WriteLine("Json has no \'@metadata\' attribute  ,and cannot sync (UPDATE/CREATE) with DB.");
+                }
+            }
+        }
+
+        //Convert json to Bson.
+        public static BlittableJsonReaderObject ParseJson(JsonOperationContext context, string json)
+        {
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            {
+                ValueTask<BlittableJsonReaderObject> b = context.ReadForMemoryAsync(stream, "json");
+                return b.Result;
+            }
         }
     }
 }
+
 
 
 
